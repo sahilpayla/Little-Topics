44e0d635b768601acfa2127e6a9fe325a693a5f8
Shraddhanand <1shraddhanand@gmail.com>
2023-03-27 10:33:36 +0530

    neumphorism

--- shadow-models/src/App.js
+++ shadow-models/src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>Different Box Shadows</h1>
+      {/* <h1>Different Box Shadows</h1> */}
 
-
+      <div className="container">
+        <div className="box shadow1">shadow1</div>
+        <div className="box shadow2">shadow2</div>
+        <div className="box shadow3">shadow3</div>
+        <div className="box shadow4">shadow4</div>
+      </div>
     </>
   );
 }
