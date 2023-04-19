e557d04033d5ae0fbe93bb9e1b2ef989d103ccd7
Shraddhanand <1shraddhanand@gmail.com>
2023-04-19 08:27:26 +0530

    d

--- todo-redux/src/App.js
+++ todo-redux/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <div className="app">
-        hellow redux
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="app">
+                hellow redux
+            </div>
+        </>
+    );
 }
 
 export default App;
