8fd1e59d8a7dbb52474f17314461438fb12ad9fe
Shraddhanand <1shraddhanand@gmail.com>
2023-04-22 08:36:49 +0530

    basic counter app with useState hook

---
+++ counter-app-material-ui/src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import './App.css';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick0 = () => {
+    setCounter(0)
+    alert('Reset Button Clicked')
+  }
+
+  const handleClick1 = () => {
+    if (counter === 0) {
+      setCounter(0);
+      alert('Ohh bas Kar baiii')
+    }
+    else {
+      setCounter(counter - 1)
+    }
+  }
+
+  const handleClick2 = () => {
+    if (counter === 999) {
+      setCounter(999);
+      alert('Ohh bas Kar baiii')
+    }
+    else {
+      setCounter(counter + 1)
+    }
+  }
+
+  return (
+    <>
+      <div className="background">
+        <div className="main__container">
+          <div className="display__section">
+            {counter}
+            <button onClick={handleClick0} className="reset">Reset</button>
+          </div>
+          <div className="btn__section">
+            <button onClick={handleClick1} className="left__btn">-</button>
+            <div onMouseOver={handleClick2} onMouseLeave={handleClick2} className="right__btn">+</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
