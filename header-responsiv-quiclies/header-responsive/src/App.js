646592779e26339406ed402a9d7f56ef9ef6eecd
Shraddhanand <1shraddhanand@gmail.com>
2023-04-30 09:56:25 +0530

    adding header

--- header-responsiv-quiclies/header-responsive/src/App.js
+++ header-responsiv-quiclies/header-responsive/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './screens/Home'
+import Skills from './screens/Skills'
+import Contact from './screens/Contact'
+import About from './screens/About'
 
 const App = () => {
   return (
     <>
-        <Routes>
-          <Route exact path='/' element={<Navbar />} />
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/contact' element={<Contact />} />
+        <Route exact path='/skills' element={<Skills />} />
+      </Routes>
     </>
   )
 }
--- header-responsiv-quiclies/header-responsive/src/components/Navbar.js
+++ header-responsiv-quiclies/header-responsive/src/components/Navbar.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import './navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <>
-      <h1>hello world</h1>
+      <div className="navbar">
+        <h3 className='logo'>Logo</h3>
+        <ul>
+          <Link to='/'>
+            <li className='nav-items'>Home</li>
+          </Link>
+          <Link to='/about'>
+            <li className='nav-items'>About</li>
+          </Link>
+          <Link to='/contact'>
+            <li className='nav-items'>Contact</li>
+          </Link>
+          <Link to='/skills'>
+            <li className='nav-items'>Skills</li>
+          </Link>
+        </ul>
+      </div>
     </>
   )
 }
