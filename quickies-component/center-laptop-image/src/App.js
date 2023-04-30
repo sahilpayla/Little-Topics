8dd76e9a50784a214459802d51849f873df69ec7
Shraddhanand <1shraddhanand@gmail.com>
2023-04-30 13:05:07 +0530

    adding css queries

--- quickies-component/center-laptop-image/src/App.js
+++ quickies-component/center-laptop-image/src/App.js
@@ -5,10 +5,14 @@ import screen from './Images/laptop.png';
 function App() {
   return (
     <>
-      <div className="container">
-        <img src={laptop} alt="" />
-        <div className="inner-container">
-          <img src={screen} alt="" />
+      <div className="curve">
+        <div className="white-curve">
+          <div className="container">
+            <img src={laptop} alt="" />
+            <div className="inner-container">
+              <img src={screen} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </>
