641b4e5f0bc4a117b299651082a4f4cd23c8775d
Shraddhanand <1shraddhanand@gmail.com>
2023-05-03 11:36:59 +0530

    adding slider in reactjs Quickeies

---
+++ quickies-component/slider/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Slider from './components/Slider/Slider';
+
+function App() {
+  return (
+    <>
+      <h1>Slider in ReactJS</h1>
+
+
+      <Slider />
+    </>
+  );
+}
+
+export default App;
