293d05174c02b2904f2eaf31aad3c9acba6b931d
sahilpayla <1shraddhanand@gmail.com>
2023-06-02 18:22:48 +0530

    adding regisster api

--- hospital/backend/controllers/authController.js
+++ hospital/backend/controllers/authController.js
@@ -1,8 +1,32 @@
-const registerController = async(req, res) => {
-    try{
-    
+const express = require('express');
+const bcrypt = require("bcryptjs");
+const userModel = require("../models/userModel.js");
+
+const registerController = async (req, res) => {
+    try {
+        const existingUser = await userModel.findOne({ email: req.body.email });
+        if (existingUser) {
+            res.status(200).json({
+                success: true,
+                message: "user already in the database",
+                existingUser,
+            })
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hashedPassword;
+
+        // rest data
+        const user = new userModel(req.body);
+        await user.save();
+        return res.status(201).json({
+            success: true,
+            message: "user created ",
+            user,
+        })
     }
-    catch(err){
+    catch (err) {
+        console.log(err);
         console.log("error --> ", err.message)
         res.status(500).json({
             failure: true,
--- hospital/backend/server.js
+++ hospital/backend/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const testApi = require('./routes/testRoutes.js')
 const dotenv = require('dotenv').config();
-const colors=require('colors');
-const morgan=require('morgan');
-const cors=require('cors');
+const colors = require('colors');
+const morgan = require('morgan');
+const cors = require('cors');
 const connectDB = require("./config/db.js");
+const register = require("./routes/authRoutes.js")
+const authRoutes = require("./routes/authRoutes")
 
 // mongo database
 connectDB();
@@ -18,6 +20,7 @@ app.use(morgan('dev'))
 // routes
 const PORT = process.env.PORT || 4001;
 app.use('/', testApi)
+app.use('/api/v1/auth', authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT} in ${process.env.MODE}`.bgCyan.white)
